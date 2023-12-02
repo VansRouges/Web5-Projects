fcafd49c5d7e10d49abfebe1b291b733a6da47d4
VansRouges <evansagina57@gmail.com>
2023-12-02 21:11:30 +0100

    generated a DIDto use a in a different project

--- web5-app/index.js
+++ web5-app/index.js
@@ -2,28 +2,28 @@ import { Web5 } from '@web5/api';
 
 const { web5, did: aliceDid } = await Web5.connect();
 
-// console.log(aliceDid);
+console.log(aliceDid);
 // The way the Web5 SDK works is that it works like an API with CRUD functionality
 
 // CREATE
-const { record } = await web5.dwn.records.create({
-    data: 'Hello, Web5!, I am Vance The Conqueror, Hear ye me!',
-    message: {
-      dataFormat: 'text/plain',
-    },
-  });
+// const { record } = await web5.dwn.records.create({
+//     data: 'Hello, Web5!, I am Vance The Conqueror, Hear ye me!',
+//     message: {
+//       dataFormat: 'text/plain',
+//     },
+//   });
 
 //   console.log('writeResult', record);
 
 // READ
-const readResult = await record.data.text();
-console.log('Data Stored in DWN Records:', readResult)
+// const readResult = await record.data.text();
+// console.log('Data Stored in DWN Records:', readResult)
 
 // UPDATE
-const updateResult = await record.update({
-    data: 'Bend the knee, Web5, before your Master',
-  });
-  console.log('Updated Result:', await record.data.text())
+// const updateResult = await record.update({
+//     data: 'Bend the knee, Web5, before your Master',
+//   });
+//   console.log('Updated Result:', await record.data.text())
 
-  const deleteResult = await record.delete();
-  console.log('deleteResult:', deleteResult)
+//   const deleteResult = await record.delete();
+//   console.log('deleteResult:', deleteResult)
